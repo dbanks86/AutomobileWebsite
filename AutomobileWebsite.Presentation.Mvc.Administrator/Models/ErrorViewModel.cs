279e8df89a3e32d73179a7215be23ca9d08ebd2b
dbanks86 <dbanks86@gmail.com>
2020-04-20 21:16:39 -0400

    Initial commit of MVC Administrator project

---
+++ AutomobileWebsite.Presentation.Mvc.Administrator/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AutomobileWebsite.Presentation.Mvc.Administrator.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
